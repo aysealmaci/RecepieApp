2971ffdfe35c6d2506f387f7572ac3c27847517f
Ayşe ALMACI <97750042+aysealmaci@users.noreply.github.com>
2022-12-24 22:40:25 +0300

    Update main.dart

--- recipeappui/lib/main.dart
+++ recipeappui/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Recepie App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
